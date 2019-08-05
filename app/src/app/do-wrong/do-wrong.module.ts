80dc10234cb91412937baaf803ca03d352a50ff8
zixing <zinianly@163.com>
2019-08-05 13:38:00 +0800

    no message

--- app/src/app/do-wrong/do-wrong.module.ts
+++ app/src/app/do-wrong/do-wrong.module.ts
@@ -1,26 +1,29 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {Routes, RouterModule} from '@angular/router';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { DoWrongPage } from './do-wrong.page';
+import {DoWrongPage} from './do-wrong.page';
+import {ShareModule} from '../share/share.module';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: DoWrongPage
-  }
+    {
+        path: '',
+        component: DoWrongPage
+    }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
-  declarations: [DoWrongPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        ShareModule,
+        RouterModule.forChild(routes)
+    ],
+    declarations: [DoWrongPage]
 })
-export class DoWrongPageModule {}
+export class DoWrongPageModule {
+}
